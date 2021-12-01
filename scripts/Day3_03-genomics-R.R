f16a24f23295a998bddc266b81f922039cdf3fd2
shannonmargaret <shannon.soucy@dartmouth.edu>
2021-12-01 15:59:30 -0500

    Update Day3_03-genomics-R.R

--- scripts/Day3_03-genomics-R.R
+++ scripts/Day3_03-genomics-R.R
@@ -1,12 +1,9 @@
 ## 03-genomics-w-R
 
-########################################
+
 library(BSgenome)
-
 available.genomes()
-
-########################################
-# use the DNAString contructor function to create a 10 letter DNA sequence
+#########################
 seq <- DNAString(x="AGCT", start=1, nchar=NA)
 seq
 
@@ -16,16 +13,17 @@ length(seq)
 # show the structure of the DNAString object
 str(seq)
 
-########################################
+#########################
 # print DNA alphabet to see what it returns
 DNA_ALPHABET
 
-########################################
+#########################
+# print iupac code to console
 IUPAC_CODE_MAP
 
-########################################
+#########################
 # randomly sample from specific characters in DNA_ALPHABET to create a longer sequence
-seq = sample(DNA_ALPHABET[c(1:4, 16)], size=100, replace=TRUE)
+seq = sample(DNA_ALPHABET[c(1:4)], size=100, replace=TRUE)
 seq
 
 # use the paste command to collapse the characters into a single string
@@ -36,7 +34,7 @@ seq
 seq.dnastring <- DNAString(seq)
 seq.dnastring
 
-########################################
+#########################
 # confirm how long it is
 length(seq.dnastring)
 
@@ -52,7 +50,7 @@ dinucleotideFrequency(seq.dnastring, as.prob=TRUE)
 # or even trinucleotides
 trinucleotideFrequency(seq.dnastring, as.prob=TRUE)
 
-########################################
+#########################
 # subset the sequence for 10 specific bases of interest  
 seq.dnastring[10:19]
 
@@ -70,10 +68,8 @@ reverseComplement(subseq(seq.dnastring, 1, 10))
 
 # translate our DNA sequence
 translate(seq.dnastring)
-# Error in .Call2("DNAStringSet_translate", x, skip_code, dna_codes[codon_alphabet],  : 
-# not a base at pos 3
 
-########################################
+#########################
 # remove the old single sequence from our global R environment
 rm(seq)
 
@@ -91,6 +87,7 @@ length(seq.dnass)
 
 # use the constructor function DNAStringSet to make the DNAStringSet object with your sequences
 dna.st.set = DNAStringSet(seq.dnass)
+dna.st.set
 
 # how long is the DNAStringSet object
 length(seq.dnass)
@@ -98,9 +95,10 @@ length(seq.dnass)
 # name all your sequences
 names(seq.dnass) = paste("barcode-", 1:5, sep="")
 seq.dnass
+#########################
+# assign the genome to a variable using getBSgenome() (you need to have the package for the BSgenome you are trying to load already installed)
+#BiocManager::install("BSgenome.Mmusculus.UCSC.mm10")
 
-########################################
-# assign the genome to a variable using getBSgenome() 
 genome <- getBSgenome("BSgenome.Mmusculus.UCSC.mm10")
 genome
 
@@ -109,19 +107,18 @@ str(genome)
 
 # print the metadata for the genome
 metadata(genome)
-#Error in (function (classes, fdef, mtable)  : 
-#unable to find an inherited method for function ‘metadata’ for signature ‘"BSgenome"’
 
-########################################
+#########################
+BiocManager::install("BSgenome.Mmusculus.UCSC.mm10.masked")
 genome.m <- getBSgenome("BSgenome.Mmusculus.UCSC.mm10.masked")
 class(genome.m)
 genome.m
 
-# unmasked genome
+# unmaksed genome
 class(genome)
 genome
 
-########################################
+#########################
 # return basic sequence information summary
 seqinfo(genome.m)
 
@@ -131,52 +128,54 @@ genome.m$chr1
 # unmasked genome
 seqinfo(genome)
 genome$chr1
-
-########################################
+#########################
 rm(genome)
 
 genome <- getBSgenome("BSgenome.Mmusculus.UCSC.mm10.masked")
-
-########################################
+#########################
 # assign chr 1
 chr1 <- genome$chr1
 
 # confirm how long it is
 length(chr1)
 
+# subset it
+subseq(chr1, 1, 100)
+subseq(chr1, 100498, 100598)
+
 # what is the frequency of each base in your sequence
 alphabetFrequency(chr1, baseOnly=TRUE, as.prob=TRUE)
 
 # what is the frequency of your favourite base
 letterFrequency(chr1, "A", as.prob=TRUE)
-
-######################################## 
+#########################
 # we need to establish a vector describing what the extra extended BED columns are
 extraCols_narrowPeak <- c(signalValue = "numeric", pValue = "numeric",
                           qValue = "numeric", peak = "integer")
 
 # read in peaks
-bed <- import("CTCF-forebrain-mm10.bed", extraCols = extraCols_narrowPeak, format="BED")
-bed
+bed <- import("CTCF-forebrain-mm10.bed",
+              format="BED",
+              extraCols = extraCols_narrowPeak,
+              genome = "mm10")
 
-# extract sequences for peak regions and print to console -first command take a minute or two
+# extract sequences for peak regions and print to console
 ctcf_seqs <- getSeq(genome, bed)
 ctcf_seqs
 
-########################################
+#########################
 # calculate nucleotide freqs.
 nt_freqs <- alphabetFrequency(ctcf_seqs, baseOnly=TRUE, as.prob=TRUE)
 
 # calculate mean value for nucleotide freqs across all peaks
 round(apply(nt_freqs, 2, mean), digits=2)
-
-########################################
+#########################
 hist(width(ctcf_seqs),
      col = "darkgray",
      xlab = "Peak width (bp)",
      main = "CTCF peak width distribution")
 
-########################################
+#########################
 # resize the regions from the BED file
 bed_centered <- resize(bed, width = 400, fix = "center")
 bed_centered
@@ -188,66 +187,9 @@ width(bed_centered)
 ctcf_seqs_cent <- getSeq(genome, bed_centered)
 ctcf_seqs_cent
 
-########################################
+#########################
+# add names to peaks in ctcf_seqs so that FASTA entries have names
+names(ctcf_seqs) <- paste0(seqnames(bed), ":", start(bed), "-", end(bed))
+
 # export peaks to FASTA file
 writeXStringSet(ctcf_seqs, file="CTCF-peaks-resized.fa")
-
-######################################## Started here again 
-fasta.file <- "a.queenslandica.fasta"
-a.queen <- readDNAStringSet(fasta.file, "fasta")
-a.queen
-
-########################################
-# confirm how long it is
-length(a.queen)
-
-# what is the frequency of each base in your sequence
-base.freqs <- alphabetFrequency(a.queen, baseOnly=TRUE, as.prob=TRUE)
-base.freqs
-
-# what is the frequency of your favourite base
-a.freqs <- letterFrequency(a.queen, "A", as.prob=TRUE)
-a.freqs
-
-########################################
-# return all matches in a DNAString subject sequence to a query pattern
-matchPattern("ATG", a.queen[[1]])
-
-# only return how many counts were found
-countPattern("ATG", a.queen[[1]])
-
-# what happens if we remove the indexing of the DNAStringSet object 'a.queen'? Why?
-matchPattern("ATG", a.queen)
-
-# match a query sequence against multiple sequences that you want to search in  
-vmatch <- vmatchPattern("ATG", a.queen)
-vmatch
-
-# look at the structure of this object
-str(vmatch)
-
-# extract the IRanges for matches in the first subject sequence
-vmatch[[1]]
-
-########################################
-# create a DNAStringSet of the stop codons
-stop.codons <- DNAStringSet(c("TAG", "TAA", "TGA"))
-
-# create a dictionary of patterns (PDict class object) that you want to search your sequences for
-stop.codons.dict <- PDict(stop.codons)
-
-# search in the first coding sequence
-match1 <- matchPDict(stop.codons.dict, a.queen[[1]])
-match1
-match1[[3]]
-
-# use a loop to search for stop codons in all our coding sequences
-matches <- list()
-for(i in 1:length(a.queen)){
-  matches[[i]] <- matchPDict(stop.codons.dict, a.queen[[i]])
-}
-length(matches)
-str(matches)
-matches[[4]]
-matches[[4]][[3]]
-
